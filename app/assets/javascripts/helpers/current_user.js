8801b39f2f3a74616522b62cf18f73e544cf8663
Hans <hanstrax@gmail.com>
2013-07-25 21:40:55 -0400

    clening up

--- app/assets/javascripts/helpers/current_user.js
+++
@@ -1,9 +0,0 @@
-Ember.Handlebars.registerBoundHelper('current_user', function() {
-  /*user_id = EmberBlog.user_id;
-  if(typeof(user_id) == "number"){
-    return (user_id > 0);
-  }else{
-    return false;
-  }*/
-  return EmberBlog.current_user;
-}) //.property('EmberBlog.user_id');
